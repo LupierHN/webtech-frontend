1e59e322e61ae57013aa3b0e7314092401ebd91a
Lukas Pierchalla <github@l.pierchalla.de>
2024-12-23 13:09:34 +0100

    keine Ahnung

---
+++ src/components/DocEditorAPI.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+config: {
+  placeholderText: 'Edit Your Content Here!',
+  charCounterCount: false
+}
+</script>
+
+<template>
+  <froala id="edit" :tag="'textarea'" :config="config" v-model:value="model"></froala>
+</template>
+
+<style scoped>
+
+
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,19 @@ import { createApp } from 'vue'
 import axios from 'axios'
 import App from './App.vue'
 import router from './router'
+import VueFroala from 'vue-froala-wysiwyg'
 import { logout } from '@/userUtils'
 import { Bars3Icon, BellIcon, XMarkIcon, UserCircleIcon, FolderIcon } from '@heroicons/vue/24/outline'
+// Import Froala Editor
+import 'froala-editor/js/plugins.pkgd.min.js'
+// Import third party plugins
+import 'froala-editor/js/third_party/embedly.min'
+import 'froala-editor/js/third_party/font_awesome.min'
+import 'froala-editor/js/third_party/spell_checker.min'
+import 'froala-editor/js/third_party/image_tui.min'
+// Import Froala Editor css files
+import 'froala-editor/css/froala_editor.pkgd.min.css'
+import 'froala-editor/css/froala_style.min.css'
 
 const app = createApp(App)
 
@@ -43,6 +54,7 @@ axios.interceptors.response.use(
   }
 )
 
+app.use(VueFroala)
 app.use(router)
 
 app.mount('#app')
